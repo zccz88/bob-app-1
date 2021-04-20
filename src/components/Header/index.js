8013808fb256123d25fbe7e582ce7aae6f25e598
BAEK SANGHEUN <170410_@naver.com>
2021-04-21 00:49:22 +0900

    [UPDATE] 헤더 및 프로필 페이지 로그아웃 버튼 추가

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,42 +1,51 @@
+import { AppBar, IconButton, Toolbar, MenuIcon, Typography, Button } from "@material-ui/core";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, NavLink } from "react-router-dom";
-import "./style.css";
+// import "./style.css";
 import { logout } from "../../actions";
 
-const Header = (props) => {
+const Header = ({ title }) => {
   const auth = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
   return (
-    <header className="header">
-      <div style={{ display: "flex" }}>
-        <div className="logo">Web Messenger</div>
-        {!auth.authenticated ? (
-          <ul className="leftMenu">
-            <li>
-              <NavLink to={"/login"}>Login</NavLink>
-            </li>
-            <li>
-              <NavLink to={"/signup"}>Sign up</NavLink>
-            </li>
-          </ul>
-        ) : null}
-      </div>
-      <div style={{ margin: "20px 0", color: "#fff", fontWeight: "bold" }}>
-        {auth.authenticated ? `Hi ${auth.firstName} ${auth.lastName}` : ""}
-      </div>
-      <ul className="menu">
-        {auth.authenticated ? (
-          <li>
-            <Link to={"#"} onClick={() => dispatch(logout(auth.uid))}>
-              Logout
-            </Link>
-          </li>
-        ) : null}
-      </ul>
-    </header>
+    <AppBar position="static">
+      <Toolbar color="inherit">
+        <h2 className="title">{title}</h2>
+      </Toolbar>
+    </AppBar>
   );
+
+  // return (
+  //   <header className="header">
+  //     <div style={{ display: "flex" }}>
+  //       <div className="logo">Web Messenger</div>
+  //       {!auth.authenticated ? (
+  //         <ul className="leftMenu">
+  //           <li>
+  //             <NavLink to={"/login"}>Login</NavLink>
+  //           </li>
+  //           <li>
+  //             <NavLink to={"/signup"}>Sign up</NavLink>
+  //           </li>
+  //         </ul>
+  //       ) : null}
+  //     </div>
+  //     <div style={{ margin: "20px 0", color: "#fff", fontWeight: "bold" }}>
+  //       {auth.authenticated ? `Hi ${auth.firstName} ${auth.lastName}` : ""}
+  //     </div>
+  //     <ul className="menu">
+  //       {auth.authenticated ? (
+  //         <li>
+  //           <Link to={"#"} onClick={() => dispatch(logout(auth.uid))}>
+  //             Logout
+  //           </Link>
+  //         </li>
+  //       ) : null}
+  //     </ul>
+  //   </header>
+  // );
 };
 
 export default Header;
--- src/pages/BoardPage/index.js
+++ src/pages/BoardPage/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import Header from "../../components/Header";
 
 const BoardPage = () => {
   return (
     <Layout>
+      <Header title={"밥 친구 게시판"} />
       <h1>게시판</h1>
     </Layout>
   );
