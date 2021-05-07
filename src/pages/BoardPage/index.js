4da7d4b708117e8e777fdc83ef41ea46211dfe31
Kangjihoon <vhxpvh9990@gmail.com>
2021-05-07 18:14:10 +0900

    필터링

--- src/pages/BoardPage/index.js
+++ src/pages/BoardPage/index.js
@@ -4,13 +4,21 @@ import Header from "../../components/Header";
 import "./styles.css";
 import { firestore } from "../../fbase";
 import { Link } from "react-router-dom";
-import { SearchSharp } from "@material-ui/icons";
+import axios from "axios";
 
 const BoardPage = ({ history }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [storageLength, setStorageLength] = useState(localStorage.length);
   const [search, setSearch] = useState("");
+  const [click, setClick] = useState([]);
   const [posts, setPosts] = useState([]);
+  const loadedCoords = localStorage.getItem("coords");
+  const parsedCoords = JSON.parse(loadedCoords);
+  const lat = parsedCoords.latitude;
+  const long = parsedCoords.longitude;
+  const [cityName, setCityName] = useState("");
+  const [dongName, setDongName] = useState("");
+  const [fullName, setfullName] =useState("");
 
   const getPosts = async () => {
     const db = await firestore.collection("board").get();
@@ -56,25 +64,47 @@ const BoardPage = ({ history }) => {
     console.log(storageLength);
     return () => setIsLoading(false);
   }, [storageLength, handleGeoSuccess]);
+  
+  useEffect(() => {
+    getLocation();
+  }, []);
 
-
+  const getLocation = async () => {
+    const key = '30ee4841c133d7b69f5287ba46b3c9f3';
+    const url = `https://dapi.kakao.com/v2/local/geo/coord2address.json?x=${long}&y=${lat}&input_coord=WGS84`;
+    const res = await axios.get(url, {
+      headers: { Authorization: "KakaoAK " + key },
+    });
+    setCityName(res.data.documents[0].address.region_2depth_name);
+    setDongName(res.data.documents[0].address.region_3depth_name);
+    setfullName(res.data.documents[0].address.region_2depth_name + ' ' +  res.data.documents[0].address.region_3depth_name);
+  };
 
   return (
     <Layout>
       <Header title={"밥 친구 게시판"} />
       <div className="container">
         <input type="text" placeholder="Input title or location..." onChange={(e) => setSearch(e.target.value)}/>
+        <div>
+          <button onChange={(e)=>setClick(e.target.value)} onClick={()=>{
+          const count = 0;
+          setClick(count);
+        }}>모든 게시글</button>
+        </div>
+        <div>
+          <button onChange={(e)=>setClick(e.target.value)} onClick={()=>{
+          const count = 1;
+          setClick(count);
+          }}>현재위치게시글</button>
+        </div>
         {posts.map((post)=>{
-          if(search==""){
+          const full = fullName.toLowerCase().indexOf(search.toLowerCase())>=0;
+          const title = post.title.toLowerCase().indexOf(search.toLowerCase())>=0;
+          const city = post.cityName.toLowerCase().indexOf(search.toLowerCase())>=0;
+          const dong =  post.dongName.toLowerCase().indexOf(search.toLowerCase())>=0;
+          if(click == 0 && (title || full || city || dong)){
             return <Link key={post.id} to={`/board/${post.id}`}> {post.title} </Link>;
-          }else if
-          (
-           post.title.toLowerCase().indexOf(search.toLowerCase())>=0
-           ||
-           post.cityName.toLowerCase().indexOf(search.toLowerCase())>=0
-           ||
-           post.dongName.toLowerCase().indexOf(search.toLowerCase())>=0
-           ){
+          }else if(cityName==post.cityName && (title || full || city || dong)){
             return <Link key={post.id} to={`/board/${post.id}`}> {post.title} </Link>;
           }
         })}
@@ -93,17 +123,31 @@ const BoardPage = ({ history }) => {
 };
 
 export default BoardPage;
+
 /*
-          if(search == ""){
+        {posts.map((post)=>{
+          if(search==""){
             return <Link key={post.id} to={`/board/${post.id}`}> {post.title} </Link>;
-          }else if(search == post.title || search == post.cityName || search == post.dongName){
+          }else if
+          (
+           post.title.toLowerCase().indexOf(search.toLowerCase())>=0
+           ||
+           post.cityName.toLowerCase().indexOf(search.toLowerCase())>=0
+           ||
+           post.dongName.toLowerCase().indexOf(search.toLowerCase())>=0
+           ){
             return <Link key={post.id} to={`/board/${post.id}`}> {post.title} </Link>;
           }
+        })}
 */
 /*
-          if(search==""){
-            return post.title
-          }else if(post.title.indexOf(search)>=0){
-            return post.title
-          }
+          (
+            fullName.toLowerCase().indexOf(search.toLowerCase())>=0//남양주시 와부읍 도곡리
+            ||
+            post.title.toLowerCase().indexOf(search.toLowerCase())>=0//제목
+            ||
+            post.cityName.toLowerCase().indexOf(search.toLowerCase())>=0//남양주시
+            ||
+            post.dongName.toLowerCase().indexOf(search.toLowerCase())>=0//와부읍 도곡리
+          )
 */
